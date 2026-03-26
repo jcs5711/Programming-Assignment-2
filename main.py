4610d55a67bf869e8d7a96d948b3f8c17122994a
jcs5711 <jcs57@kent.ac.uk>
2026-03-26 21:32:26 +0000

    Because i made a file and added some of the functions there i could get rid of them from the main.py file while also making sure i called those function from the file i had mved them to. I also did some touch ups on some of the other lines of code so it would run smoother and look neater.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 file = "expenses.txt"
 
 from expense import Expense
+from expense_utils import load_expenses, save_expenses, view_expenses, total_expenses
 
 def welcome():
     print("Welcome to your Expense Tracker!")
@@ -12,69 +13,34 @@ def welcome():
     print("5. Exit")
     print("---------------------------------")
 
-def load_expenses():
-    expenses = []
-    try:
-        with open(file, 'r') as f:
-            for line in f:
-                name, amount = line.strip().split(',')
-                expenses.append({"name": name, "amount": float(amount)})
-    except FileNotFoundError:
-        print("File doesnt exist.")
-    except Exception as e:
-        print(e)
-    return expenses
-
-def save_expenses(expenses):
-    with open(file,'w') as f:
-        for e in expenses:
-            f.write(f"{e['name']},{e['amount']}\n")
-
 def add_expense(expenses):
     try:
         name = input("Enter expense name: ")
         amount = float(input("Enter expense amount: "))
-        expenses.append({"name": name, "amount": amount})
+        expense = Expense(name, amount)
+        expenses.append(expense)
         save_expenses(expenses)
         print("Expense added successfully!")
-
-    except ValueError:
-        print("Invalid Input. Enter a valid amount.")
-    
+    except ValueError as e:
+        print(f"Invalid Input. Enter a valid amount. {e}")
     except Exception as e:
         print(f"An error has occurred: {e}")
-
-def view_expenses(expenses):
-    print("\nExpenses:")
-    i = 1
-    for e in expenses:
-        print(f"{i}. {e['name']} = ${e['amount']}")
-        i += 1
-    print()
-
-def total_expenses(expenses):
-    total = 0 
-    for e in expenses: 
-        total += e["amount"]
-
-    print(f"Your Total Expenses are ${total}")
 
 def remove_expense(expenses):
     if not expenses:
         print("\n There are no expenses to remove.")
         return
-    
     view_expenses(expenses)
     try:
         number = int(input("Enter the number of the expense to remove: "))
         if number >= 1 and number <= len(expenses):
             removed = expenses.pop(number - 1)
             save_expenses(expenses)
-            print(f"Removed expense: {removed['name']} - {removed['amount']}")
+            print(f"Removed expense: {removed}")
         else: 
             print("Invalid number.")
-    except ValueError:
-        print("Enter a valid number. \n")
+    except ValueError as e:
+        print(f"Enter a valid number. \n")
 
 def main():
     welcome()
@@ -95,6 +61,5 @@ def main():
         else:
             print("Invalid Input")  
 
-
-main()
-    
+if __name__ == "__main__":
+    main()
