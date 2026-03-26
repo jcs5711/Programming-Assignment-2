6b4b6a9e6a354cff71600a3a82dd479e090e2abc
jcs5711 <jcs57@kent.ac.uk>
2026-03-26 15:40:52 +0000

    Started making all the options in the menu so when they are chosen they can perform their spefic job and fixing formatting issues

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ file = "expenses.txt"
 
 def welcome():
     print("Welcome to your Expense Tracker!")
-    print("--------------------------------")
+    print("---------------------------------")
     print("1. Add Expense")
     print("2. View Expenses")
-    print(3. Total Expenses)
+    print("3. Total Expenses")
     print("4. Remove Expense")
     print("5. Exit")
 
@@ -22,6 +22,37 @@ def load_expenses():
         print(e)
     return expenses
 
-def add_expense():
+def save_expenses(expenses):
+    with open(file,'w') as f:
+        for e in expenses:
+            f.write(f"{e['name']},{e['amount']}\n")
+
+def add_expense(expenses):
     try:
         name = input("Enter expense name: ")
+        amount = float(input("Enter expense amount: "))
+        expenses.append((name, amount))
+        save_expenses(expenses)
+        print("Expense added successfully!")
+
+    except ValueError:
+        print("Invalid Input. Enter a valid amount.")
+    
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+
+def view_expenses(expenses):
+    print("\nExpenses:")
+    i = 1
+    for e in expenses:
+        print(f"{i}. {e['name']}: ${e['amount']}")
+        print()
+
+def total_expenses(expenses):
+    total = 0 
+    for e in expenses: 
+        total += e["amount"]
+
+    print(f"Your Total Expenses are ${total}")
+
+
